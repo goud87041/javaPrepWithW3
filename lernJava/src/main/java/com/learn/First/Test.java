c5c1881d66774d2ce41679cf1b2fc07beccda027
abhishek goud <goud87041@gmail.com>
2026-03-02 08:25:36 +0530

    Add Test and Main classes with basic structure and functionality

---
+++ lernJava/src/main/java/com/learn/First/Test.java
@@ -0,0 +1,9 @@
+package main.java.com.learn.First;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 3 , b= 5 ;
+        int c = a + b;
+        System.out.println("value of c "+c);
+    }
+}
---
+++ lernJava/src/main/java/com/learn/Main.java
@@ -0,0 +1,7 @@
+package main.java.com.learn;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
